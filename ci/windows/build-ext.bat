1a22582274fd9f5cfd40c49387f553d3f5abc7c9
Gold Development <admin@gold-development.nl>
2026-07-20 10:29:13 +0200

    ci(win): default ARCH in task shell (phpsdk clears it) when collecting DLL

--- ci/windows/build-ext.bat
+++ ci/windows/build-ext.bat
@@ -37,12 +37,21 @@ if errorlevel 1 exit /b 1
 
 cd ..
 
+rem The phpsdk starter clears ARCH in the task shell, so re-default it here
+if "%ARCH%"=="" set ARCH=x64
+
 rem Collect the DLL (TS -> x64\Release_TS, NTS -> x64\Release)
 set OUTDIR=php-src\%ARCH%\Release
 if /I "%TS%"=="ts" set OUTDIR=php-src\%ARCH%\Release_TS
 
+if not exist "%OUTDIR%\php_cassandra.dll" (
+  echo php_cassandra.dll not found in %OUTDIR% - actual locations:
+  dir /s /b php-src\php_cassandra.dll
+  exit /b 1
+)
+
 if not exist artifacts mkdir artifacts
 copy /Y "%OUTDIR%\php_cassandra.dll" "artifacts\php_cassandra-%PHP_SRC_TAG%-%TS%-vs17-%ARCH%.dll"
-if errorlevel 1 ( echo php_cassandra.dll not found in %OUTDIR% & exit /b 1 )
+if errorlevel 1 exit /b 1
 
 echo Built: artifacts\php_cassandra-%PHP_SRC_TAG%-%TS%-vs17-%ARCH%.dll
